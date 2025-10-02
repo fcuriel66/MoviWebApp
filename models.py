6144a2372c7c6453efd80dd466a5cae4338be4e9
Fernando Curiel <fcuriel66@mac.com>
2025-10-02 11:44:34 +0200

    Defined User and Movie models in models.py

--- models.py
+++ models.py
@@ -1,3 +1,21 @@
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(80), nullable=False)
+
+    # one-to-many relationship
+    movies = db.relationship('Movie', backref='user', lazy=True)
+
+
+class Movie(db.Model):
+    __tablename__ = 'movies'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(100), nullable=False)
+    director = db.Column(db.String(100), nullable=False)
+    year = db.Column(db.Integer)
+    poster_url = db.Column(db.String(300))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
